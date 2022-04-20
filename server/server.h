997e7cde29de6df2bccbba9371ab6304deb53676
User <e89996550379@gmail.com>
2022-04-20 12:35:28 +0300

    add server

---
+++ server/server.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <cassert>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <unordered_map>
+#include <WinSock2.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define SIZE 1024
+
+namespace Net
+{
+class Server
+{
+private:
+	WSADATA wsa;
+	SOCKET serversocket;
+	std::string ipaddress;
+	int port;
+	char buffer[SIZE];
+	std::string message;
+	struct sockaddr_in info;
+	int infoLengh;
+	int reciveleng;
+public:
+	Server(int, std::string);
+	~Server();
+public:
+	void start();
+	void stop();
+	void init();
+private:
+	void receive();
+	void send();
+	void proccess();
+};
+
+}
